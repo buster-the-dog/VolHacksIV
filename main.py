47cbc037a5d3934c81abd6390a7a3f1ecf1ccfd0
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 10:48:18 -0400

    feedback loop

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ import time
 import Adafruit_DHT
 
 def main():
-  humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+  targetTemp = 50
+  targetHumid = 30
+  dryingTime = -1
+  while (true)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    #turn heater/fan on
+    heaterPort = 2
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(heaterPort, GPIO.OUT)
+    GPIO.output(heaterPort, GPIO.HIGH)
+    print humidity, ' ', temperature
+    if temperature >= targetTemp
+      #turn heater off
+      #log time and wait for it to stay for a while, if humidity is still too low raise target temp
+      GPIO.output(heaterPort, GPIO.LOW)
+      if dryingTime == -1
+        dryingTime = time.clock()
+      if dryingTime == 3600 * 3
+        #drying is done here, return
+        return dryingTime
   return humidity, temperature
 
 if __name__ == "__main__":
