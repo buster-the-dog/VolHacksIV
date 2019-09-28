7f416879d32455806d5b530c18e006a95fab2f2d
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 12:01:54 -0400

    pid

--- main.py
+++ main.py
@@ -7,25 +7,18 @@ def main():
   targetTemp = 50
   targetHumid = 30
   dryingTime = -1
+  heaterPort = 2
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(heaterPort, GPIO.OUT)
+  o = GPIO.PWM(heaterPort, 30)
+  o.start(100)
+  
   try:
     while True:
       humidity, temperature = Adafruit_DHT.read_retry(11, 4)
       #turn heater/fan on
-      heaterPort = 2
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(heaterPort, GPIO.OUT)
-      GPIO.output(heaterPort, GPIO.HIGH)
-      print humidity, ' ', temperature
-      if temperature >= targetTemp:
-        #turn heater off
-        #log time and wait for it to stay for a while, if humidity is still too low raise target temp
-        GPIO.output(heaterPort, GPIO.LOW)
-        if dryingTime == -1:
-          dryingTime = time.clock()
-        if dryingTime == 3600 * 3:
-          #drying is done here, return
-          return dryingTime
-    return humidity, temperature
+      e = (targetTemp - temperature) / targetTemp
+      o.ChangeDutyCycle(e * 100)
   except KeyboardInterrupt:
     GPIO.output(heaterPort, GPIO.LOW)
     GPIO.cleanup()
