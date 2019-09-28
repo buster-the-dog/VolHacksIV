5713a5cbffc225f780c0ffa0c3adc7a4d2bfa2ed
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 13:03:34 -0400

    cli fix 1

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def main():
   o = GPIO.PWM(heaterPort, 45)
   o.start(100)
   
+  print sys.argv[1]
+  
+  if len(sys.argv) < 1:
+    return "usage: python main.py [targetTemp]"
+  
   try:
     while True:
       humidity, temperature = Adafruit_DHT.read_retry(11, 4)
       #turn heater/fan on
       e = (targetTemp - temperature) / targetTemp
       print e, temperature
-      print input()
       if e < 0:
         e = 0
       o.ChangeDutyCycle(e * 100)
