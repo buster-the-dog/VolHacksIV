c7e945bcdc080d744ab975461673bb3f0381bec3
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 14:27:20 -0400

    added test code

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
       humidity, temperature = Adafruit_DHT.read_retry(11, 4)
       #turn heater/fan on
       e = (targetTemp - temperature) / (targetTemp - initTemp)
-      print e, temperature
+      print e, temperature, dryingTimeStart
       if e < 0:
         e = 0
       if e > 1:
