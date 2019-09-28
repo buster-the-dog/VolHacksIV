23da75c53eda27e9483336f1333db042df33050a
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 14:59:33 -0400

    adds humidity output

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
       humidity, temperature = Adafruit_DHT.read_retry(11, 4)
       #turn heater/fan on
       e = (targetTemp - temperature) / (targetTemp - initTemp)
-      print e, temperature, targetTemp
+      print e, temperature, targetTemp, humidity
       if e < 0:
         e = 0
       if e > 1:
