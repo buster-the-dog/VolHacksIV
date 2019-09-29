b7941299a6db306b75aac75be557e116a5a3de82
LAPTOP-S9DE3USU\buster <sirsamuelhatt@gmail.com>
2019-09-28 23:55:13 -0400

    test serial

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
   GPIO.setup(heaterPort, GPIO.OUT)
   o = GPIO.PWM(heaterPort, 45)
   o.start(100) 
-  s = serial.Serial('tty1', 9600, timeout = 1)
+  s = serial.Serial('/dev/tty1', 9600, timeout = 1)
   
   if len(sys.argv) < 2:
     return "usage: python main.py [targetTemp] [heatingTime]"
